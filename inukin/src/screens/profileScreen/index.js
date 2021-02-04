77aa991cbbfbc76c8225cba776e27b63f578182b
DESKTOP-7FK9A7K\azhar <azharuddinmullah219@gmail.com>
2021-02-04 21:28:38 +0530

    new profile screen

---
+++ inukin/src/screens/profileScreen/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const ProfileScreen = () => {
+    return <Text>Helo</Text>
+};
+
+const styles = StyleSheet.create({});
+
+export default ProfileScreen;
